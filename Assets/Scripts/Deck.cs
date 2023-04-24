fec7ee228ec0cf96492af59357b651d3fae84b7b
DiegoMe11 <diegomepascu@gmail.com>
2023-04-24 16:00:19 +0200

    InitCardValues

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -34,6 +34,24 @@ public class Deck : MonoBehaviour
          * En principio, la posición de cada valor se deberá corresponder con la posición de faces. 
          * Por ejemplo, si en faces[1] hay un 2 de corazones, en values[1] debería haber un 2.
          */
+
+        for (int i = 0; i < values.Length; i++) //Damos valores a cada carta
+        {
+            switch (i % 13) //El resto
+            {
+                case 0:
+                    values[i] = 11;
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    values[i] = 10;
+                    break;
+                default:
+                    values[i] = (i % 13) + 1;
+                    break;
+            }
+        }
     }
 
     private void ShuffleCards()
